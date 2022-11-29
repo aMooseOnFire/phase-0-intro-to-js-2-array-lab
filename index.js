616c35e95f13d8720ce3f536f928869614a14dbc
amos <amosjshinkle@gmail.com>
2022-11-29 08:31:27 -0500

    commit tree 9884b06227ab1affef68f59fd63c80f67363aa30

--- index.js
+++ index.js
@@ -1,1 +1,62 @@
-// Write your solution here!
+const cats = [
+    "Milo",
+    "Otis",
+    "Garfield"
+];
+
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+  
+//destructively add Ralph
+function destructivelyAppendCat(name) {
+    cats.push("Ralph")
+}
+
+//destructively prepend Bob
+
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob")
+}
+
+//destructively remove last cat
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop()
+}
+
+//destructively remove first cat
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift()
+}
+
+//nondestructively append cat 
+
+function appendCat(name) {
+   let appendCat = [...cats, "Broom"];
+   return appendCat
+}
+
+//nondestructively prepend cat 
+
+function prependCat(name) {
+    let prependCat = ["Arnold", ...cats];
+    return prependCat
+ }
+
+ //nondestructively remove last cat
+
+ function removeLastCat(name) {
+    let removeLastCat = cats.slice(0, cats.length - 1)
+    return removeLastCat
+ }
+
+ //nondestructively remove first cat
+
+ function removeFirstCat(name) {
+    let removeFirstCat = cats.slice(1)
+    return removeFirstCat
+ }
